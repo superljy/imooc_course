d45585f34612755cbd9b483cfe38b575b90acb27
superljy <superljy0809@163.com>
2017-06-18 10:19:38 +0800

    2017-06-18updated

---
+++ bluegogo/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    // 响应式宽高
+    // 宽度
+    document.querySelector('html').style.fontSize = document.documentElement.clientWidth / 375 * 50 + 'px';
+    // 高度
+    var scaleStyle = document.createElement('style');  //增加style标签
+    var scaleTemp = (750/window.innerWidth*window.innerHeight/1334).toFixed(2)*1;  //iPhone6为例，求宽高比（toFiexd是方法可把 Number 四舍五入为指定小数位数的数字。这里是两位。最后*1是为了转为整数，当然 -0也可以）
+    scaleTemp=scaleTemp>1?1:scaleTemp;  //如果宽高比大于1，就让他等于1，否则就是它自己的比例（小于1）
+    scaleStyle.innerHTML='html{font-size:'+document.documentElement.clientWidth / 375 * 50 + 'px}.auto_scale{-webkit-transform: scale('+scaleTemp+');transform: scale('+scaleTemp+');}';  //在刚创建的style标签里设置根元素的样式（这里是iphone6 ,1rem=50px;）,将他进行自由缩放
+    document.documentElement.appendChild(scaleStyle);  //增加给它的子元素
+
+    // 页面
+    $(".pg1").on("click",function(){
+        $(".pg1").removeClass("active");
+        $(".pg2").addClass("active");
+        setTimeout(function(){
+            $(".pg2").removeClass("active");
+            $(".pg3").addClass("active");
+        },12000);
+    });
+
+    
+})
+
+
