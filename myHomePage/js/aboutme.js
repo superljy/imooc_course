9171e7707398dce3b4001448c560c3acde489e40
superljy <superljy0809@163.com>
2017-07-31 23:29:29 +0800

    update Responsive layout

--- myHomePage/js/aboutme.js
+++ myHomePage/js/aboutme.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$(".nav-icon").on("click",function(){
-		$(".lSide-contact").animate({width:"toggle"},500);
+		$(".lSide-contact").animate({width:"toggle"});
 	});
 })
